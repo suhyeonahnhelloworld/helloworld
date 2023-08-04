c4ce43b863bad2a30382135ad3798ce31d0a47b3
suhyeon AHN <suahn@student.42.fr>
2023-08-04 15:44:59 +0200

    Add logging to helloworld

--- helloworld.js
+++ helloworld.js
@@ -1,18 +1,33 @@
-const http = require("http")
+const http = require("http");
+const { createLogger, transports, format } = require("winston");
+
+const version = process.env.HELLOWORLD_VERSION;
+
+const logger = createLogger({
+  format: format.combine(
+    format.timestamp({
+      format: "YYYY-MM-DD HH:mm:ss",
+    }),
+    format.printf(({ timestamp, level, message, meta }) => {
+      return `${timestamp} [${level}]: ${message} ${meta ? JSON.stringify(meta) : ""}`;
+    })
+  ),
+  transports: [new transports.Console()],
+});
 
 const server = http.createServer(function (request, response) {
+  // Send the HTTP header
+  // HTTP Status: 200 : OK
+  // Content Type: text/plain
+  response.writeHead(200, { "Content-Type": "text/plain" });
 
-   // Send the HTTP header
-   // HTTP Status: 200 : OK
-   // Content Type: text/plain
-   response.writeHead(200, {'Content-Type': 'text/plain'})
-
-   // Send the response body as "Hello World"
-   response.end('Hello World From Paris\n')
-}).listen(3000)
+  // Send the response body as "Hello World"
+  response.end("Hello World From Paris\n");
+}).listen(3000);
 
 // Console will print the message
-console.log('Server running')
+logger.info("Server running", { version });
+
 
 // Wait for a minute (60,000 milliseconds) before closing the server
 //setTimeout(function () {
